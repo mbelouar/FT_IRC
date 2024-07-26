b958aa06cc197ad08984cbdc2274a32e3ec2639a
mbelouar <simobelouarraq5@gmail.com>
2024-07-26 23:06:53 +0100

    PRIVMSG is DONE

--- privmsgCmd.cpp
+++ privmsgCmd.cpp
@@ -8,12 +8,12 @@
 void Server::privmsgCmd(std::vector<std::string>& args, int fd) {
 
     if (args.size() < 1) {
-        std::string msg = "411 " + getClientNickname(fd) + " :No recipient given (PRIVMSG)\n";
+        std::string msg = "411 " + getClientNickname(fd) + " :No recipient given (PRIVMSG)\n\r";
         sendMessage(fd, msg);
         return;
     }
     else if (args.size() == 1) {
-        std::string msg = "412 " + getClientNickname(fd) + " :No text to send\n";
+        std::string msg = "412 " + getClientNickname(fd) + " :No text to send\n\r";
         sendMessage(fd, msg);
         return;
     }
@@ -24,28 +24,42 @@ void Server::privmsgCmd(std::vector<std::string>& args, int fd) {
         message += " " + args[i];
     }
 
-    std::map<std::string, client>::iterator clientIt = clients.find(recipient);
-    std::map<std::string, Channel>::iterator channelIt = channels.find(recipient);
+    std::vector<client>& clients = client::get_clients();
+    bool recipientFound = false;
+    client* recipientClient = nullptr;
 
-    if (clientIt == clients.end() && channelIt == channels.end()) {
-        // Recipient not found in both clients and channels
-        std::string msg = "401 " + getClientNickname(fd) + " " + recipient + " :No such nick/channel\n";
-        sendMessage(fd, msg);
-    }
-    else if (clientIt != clients.end()) {
-        // Recipient found as a client
-        client& recipientClient = clientIt->second;
-        std::string msg = ":" + getClientNickname(fd) + " PRIVMSG " + recipient + " :" + message + "\n";
-        sendMessage(recipientClient.get_client_pfd().fd, msg);
-    }
-    else if (channelIt != channels.end()) {
-        // Recipient found as a channel
-        std::cout << "Channel found" << std::endl;
-        Channel& channel = channelIt->second;
-        // t!tttt@88ABE6.25BF1D.D03F86.88C9BD.IP
-        std::string msg = ":" + getClientNickname(fd) + "!" + getClientNickname(fd) + "@127.0.0.1" + " PRIVMSG " + recipient + " :" + message + "\n";
-        for (std::map<int, client>::const_iterator it = channel.beginClientIter(); it != channel.endClientIter(); ++it) {
-            sendMessage(it->first, msg);
+    // Check if the recipient is a client
+    for (std::vector<client>::iterator it = clients.begin(); it != clients.end(); ++it) {
+        if (it->getNickname() == recipient) {
+            recipientClient = &(*it);   // Point to the recipient client
+            recipientFound = true;
+            break;
         }
+    }
+
+    if (!recipientFound) { 
+        // Recipient not found as a client
+        std::map<std::string, Channel>::iterator channelIt = channels.find(recipient);
+
+        if (channelIt == channels.end()) {
+            std::string msg = "401 " + getClientNickname(fd) + " " + recipient + " :No such nick/channel\n\r";
+            sendMessage(fd, msg);
+        }
+        else {
+            std::cout << "Channel found" << std::endl;
+            Channel& channel = channelIt->second;
+            std::string msg = ":" + getClientNickname(fd) + "!" + getClientNickname(fd) + "@" + getClientHostName(fd) + " PRIVMSG " + recipient + " :" + message + "\n\r";
+            for (std::map<int, client>::const_iterator it = channel.beginClientIter(); it != channel.endClientIter(); ++it) {
+                if (it->first != fd) {
+                    sendMessage(it->first, msg);
+                }
+            }
+        }
+    }
+    else {
+        // Recipient found as a client
+        std::cout << "Client found" << std::endl;
+        std::string msg = ":" + getClientNickname(fd) + " PRIVMSG " + recipient + " :" + message + "\n\r";
+        sendMessage(recipientClient->get_client_pfd().fd, msg);
     }
 }
